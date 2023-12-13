0a1a121c84b12b20b6a5645c0bc56f3ff65d0e45
gab8192 <gabrilomba57@gmail.com>
2023-12-13 12:13:10 +0100

    omegalol
    Bench 3682720

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -715,6 +715,8 @@ namespace Search {
           return singularBeta;
         else if (ttScore >= beta) // Negative extension (~18 Elo)
           extension = -1 + IsPV;
+        else if (cutNode)
+          extension = -1;
       }
 
       int oldNodesCount = nodesSearched;
