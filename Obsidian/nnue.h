c9abc6b465389bd6952619e167ed68b3851461f6
gab8192 <gabrilomba57@gmail.com>
2023-12-24 15:00:41 +0100

    lmao
    Bench 4483532

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net7.bin"
+#define EvalFile "t80-screlu181-epoch13.bin"
 
 using namespace SIMD;
 
@@ -12,7 +12,7 @@ namespace NNUE {
   using weight_t = int16_t;
 
   constexpr int FeatureDimensions = 768;
-  constexpr int TransformedFeatureDimensions = 1536;
+  constexpr int TransformedFeatureDimensions = 64;
 
   constexpr int NetworkScale = 400;
   constexpr int NetworkQA = 181;
