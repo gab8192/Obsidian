5caf57e3531238215c0e37cacc898e06335db792
gab8192 <gabrilomba57@gmail.com>
2023-12-24 18:53:40 +0100

    lmao
    Bench 3458779

--- Obsidian/evaluate.cpp
+++ Obsidian/evaluate.cpp
@@ -1,5 +1,4 @@
 #include "evaluate.h"
-#include "endgame.h"
 
 #include <iostream>
 
@@ -9,23 +8,7 @@ namespace Eval {
 
   Score evaluate(Position& pos, NNUE::Accumulator& accumulator) {
 
-    const bool whiteOnlyKing = pos.pieces(WHITE) == pos.pieces(WHITE, KING);
-    const bool blackOnlyKing = pos.pieces(BLACK) == pos.pieces(BLACK, KING);
-
-    if (whiteOnlyKing && blackOnlyKing)
-      return DRAW;
-
-    Score v;
-
-    if (whiteOnlyKing != blackOnlyKing) {
-      Color strongSide = whiteOnlyKing ? BLACK : WHITE;
-      Score strongV = evaluateEndgame(pos, strongSide);
-
-      v = (strongSide == pos.sideToMove ? strongV : -strongV);
-    }
-    else {
-      v = NNUE::evaluate(accumulator, pos.sideToMove);
-    }
+    Score v = NNUE::evaluate(accumulator, pos.sideToMove);
 
     v = Score(v * (200 - pos.halfMoveClock) / 200);
 
