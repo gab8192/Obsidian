fc75df2aef83b72a0b3e9c1b834e75272bf37e6e
gab8192 <gabrilomba57@gmail.com>
2023-12-25 19:20:27 +0100

    Revert how skipping quiets works
    Bench 3458779

--- Obsidian/movepick.h
+++ Obsidian/movepick.h
@@ -29,7 +29,7 @@ public:
     MainHistory& _mainHist, CaptureHistory& _capHist,
     Search::SearchInfo* _ss);
 
-  Move nextMove(MpStage* outStage);
+  Move nextMove(bool skipQuiets, MpStage* outStage);
 
 private:
   bool isQsearch;
