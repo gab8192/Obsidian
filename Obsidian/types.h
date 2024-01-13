87dd368bc694919dc6cf82b7c1790b85b3cbf60e
gab8192 <gabrilomba57@gmail.com>
2024-01-13 14:48:35 +0100

    More negative extension when ttmove should fail high
    Bench 6829289

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-9.29";
+const std::string engineVersion = "dev-9.30";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
