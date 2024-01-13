a3dd329c857c084abdae28cf41006f21ab0708c4
gab8192 <gabrilomba57@gmail.com>
2024-01-14 00:39:40 +0100

    Lol
    Bench 6581897

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -881,9 +881,6 @@ namespace Search {
         if (isQuiet) {
           R = lmrTable[depth][seenMoves];
           
-          // Reduce more if the expected best move is a capture
-          R += ttMoveNoisy;
-
           // Extend killer and counter move
           R -= (moveStage == KILLER || moveStage == COUNTER);
 
@@ -905,6 +902,9 @@ namespace Search {
         // Extend if this position *was* in a PV node. Even further if it *is*
         if (ttPV)
           R -= (1 + IsPV);
+
+        // Reduce more if the expected best move is a capture
+        R += ttMoveNoisy;
 
         // Reduce if evaluation is trending down
         R += !improving;
