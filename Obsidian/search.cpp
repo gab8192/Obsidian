4cb6517df2384ee697a7554cc284809c4d896c23
gab8192 <gabrilomba57@gmail.com>
2024-01-15 19:21:42 +0100

    kek
    Bench 7292231

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -185,7 +185,7 @@ namespace Search {
     ply++;
     pos.doMove(move, dirtyPieces);
 
-    TT::prefetch(pos.key);
+    //TT::prefetch(pos.key);
 
     newAcc.doUpdates(&dirtyPieces, &oldAcc);
   }
