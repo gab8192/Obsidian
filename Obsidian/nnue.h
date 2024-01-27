951ab909cb485a09d2f810de683c2fa8fd90e9ca
gab8192 <gabrilomba57@gmail.com>
2024-01-27 10:11:15 +0100

    uwu
    Bench 5735826

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net7.bin"
+#define EvalFile "plenty_net.bin"
 
 using namespace SIMD;
 
@@ -25,7 +25,7 @@ namespace NNUE {
   using weight_t = int16_t;
 
   constexpr int FeaturesWidth = 768;
-  constexpr int HiddenWidth = 1536;
+  constexpr int HiddenWidth = 1024;
 
   constexpr int NetworkScale = 400;
   constexpr int NetworkQA = 181;
