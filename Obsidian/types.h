80188f7772b304f9579d95ad26ffbc8047fe0734
gab8192 <gabrilomba57@gmail.com>
2024-02-08 00:23:52 +0100

    Safer ttpv in qsearch
    Bench 5268794

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-10.22";
+const std::string engineVersion = "dev-10.23";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
