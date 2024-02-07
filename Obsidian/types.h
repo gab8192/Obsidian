61040a8908cbe90e3568f826d17334c9a41185eb
gab8192 <gabrilomba57@gmail.com>
2024-02-07 18:44:45 +0100

    Prefetch TT in NMP
    1.5% speedup
    
    Bench 5230647

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-10.20";
+const std::string engineVersion = "dev-10.21";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
