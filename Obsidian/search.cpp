4e76a8e66faf773ffa5bad3e64e22d679e02ca84
gab8192 <gabrilomba57@gmail.com>
2024-03-03 14:19:23 +0100

    Tweak LMR
    Bench 5259960

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -890,7 +890,7 @@ namespace Search {
 
       if (depth >= 2 && seenMoves > 1 + 3 * IsRoot) {
 
-        int R = isQuiet ? lmrTable[depth][seenMoves] : 0;
+        int R = lmrTable[depth][seenMoves];
 
         // Reduce or extend depending on history of this move
         R -= history / (isQuiet ? LmrQuietHistoryDiv : LmrCapHistoryDiv);
@@ -906,7 +906,7 @@ namespace Search {
               || moveStage == MovePicker::PLAY_COUNTER);
 
         // Reduce if this is a bad capture (=> loses material)
-        R += (moveStage == MovePicker::PLAY_BAD_CAPTURES);
+        R -= !isQuiet;
 
         // Reduce more if the expected best move is a capture
         R += ttMoveNoisy;
