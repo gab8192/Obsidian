95bd0d2fe815ab71e107e37eeceef2e57c4bf3e5
gab8192 <gabrilomba57@gmail.com>
2024-03-18 15:23:18 +0100

    Remove quadratic component from stat bonus
    Elo   | -0.08 +- 1.84 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.16 (-2.25, 2.89) [-3.00, 0.00]
    Games | N: 64532 W: 15216 L: 15231 D: 34085
    Penta | [287, 7781, 16124, 7808, 266]
    
    Bench 5573340

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-11.07";
+const std::string engineVersion = "dev-11.08";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
