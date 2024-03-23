ba1becc66513bd37cea79825992ed134d63d069a
gab8192 <gabrilomba57@gmail.com>
2024-03-23 16:16:14 +0100

    uwu
    Bench 4900280

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net512_buckets.bin"
+#define EvalFile "net512_6bucket.bin"
 
 using namespace SIMD;
 
@@ -30,16 +30,16 @@ namespace NNUE {
   constexpr int HiddenWidth = 512;
 
   constexpr int KingBucketsScheme[] = {
-    0, 0, 1, 1, 1, 1, 0, 0,
-    2, 2, 2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3, 3, 3, 
-    3, 3, 3, 3, 3, 3, 3, 3, 
-    3, 3, 3, 3, 3, 3, 3, 3, 
-    3, 3, 3, 3, 3, 3, 3, 3, 
-    3, 3, 3, 3, 3, 3, 3, 3, 
-    3, 3, 3, 3, 3, 3, 3, 3
+    0, 1, 2, 3, 3, 2, 1, 0,
+    4, 4, 4, 4, 4, 4, 4, 4,
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5
   };
-  constexpr int KingBucketsCount = 4;
+  constexpr int KingBucketsCount = 6;
 
   constexpr int OutputBuckets = 8;
 
