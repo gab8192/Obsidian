941644c7198404ca955b0a68e00dd43f436fb6f2
gab8192 <gabrilomba57@gmail.com>
2024-04-09 09:15:05 +0200

    ok
    Bench 5191416

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -875,7 +875,7 @@ namespace Search {
 
         // Late move pruning. At low depths, only visit a few quiet moves
         if (! skipQuiets) {
-          if (seenMoves >= (depth * depth + LmpBase) / (2 - improving))
+          if (seenMoves >= (depth * depth + LmpBase) / (2 - improving) && !pos.checkers)
             skipQuiets = true;
         }
 
