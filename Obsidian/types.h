a81f27effef3693ad88e36afb2df57fd310d4757
gab8192 <gabrilomba57@gmail.com>
2024-04-14 22:29:24 +0200

    Simplify away refutation extension
    I got the idea of trying this because now late move pruning won't skip refutations
    
    Elo   | 1.54 +- 2.74 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.91 (-2.25, 2.89) [-2.50, 0.50]
    Games | N: 29310 W: 7026 L: 6896 D: 15388
    Penta | [97, 3438, 7458, 3562, 100]
    
    Bench 4509553

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-12.02";
+const std::string engineVersion = "dev-12.03";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
