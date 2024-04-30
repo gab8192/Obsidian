4c4eb2fa8401bdd4dc687340a91a863e04add926
gab8192 <gabrilomba57@gmail.com>
2024-04-30 09:59:50 +0200

    New net
    Elo   | 7.47 +- 5.00 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 9352 W: 2466 L: 2265 D: 4621
    Penta | [30, 1067, 2322, 1186, 71]
    
    Bench 4661823

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net37.bin"
+#define EvalFile "amogus.bin"
 
 using namespace SIMD;
 
--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-12.10";
+const std::string engineVersion = "dev-12.11";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
