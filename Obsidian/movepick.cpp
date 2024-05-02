d4798ca6507a8c53fa6dd8f77b5884f09029ace0
gab8192 <gabrilomba57@gmail.com>
2024-05-02 21:45:25 +0200

    Bench 5044366

--- Obsidian/movepick.cpp
+++ Obsidian/movepick.cpp
@@ -75,14 +75,14 @@ void MovePicker::scoreQuiets() {
     int threatScore = 0;
 
     if (pt == QUEEN) {
-      if (threats.byRook & from) threatScore += 32768;
-      if (threats.byRook & to) threatScore -= 32768;
+      if (threats.byRook & from) threatScore += 16384;
+      if (threats.byRook & to) threatScore -= 16384;
     } else if (pt == ROOK) {
-      if (threats.byMinor & from) threatScore += 16384;
-      if (threats.byMinor & to) threatScore -= 16384;
+      if (threats.byMinor & from) threatScore += 8192;
+      if (threats.byMinor & to) threatScore -= 8192;
     } else if (pt == KNIGHT || pt == BISHOP) {
-      if (threats.byPawn & from) threatScore += 16384;
-      if (threats.byPawn & to) threatScore -= 16384;
+      if (threats.byPawn & from) threatScore += 8192;
+      if (threats.byPawn & to) threatScore -= 8192;
     }
 
     quiets[i++].score =
