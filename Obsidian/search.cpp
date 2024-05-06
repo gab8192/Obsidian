dbfc53f6547001ca626e03e69591a8c6a49dd7de
gab8192 <gabrilomba57@gmail.com>
2024-05-06 14:43:54 +0200

    Tune at LTC with 40k games
    LTC:
    Elo   | 3.53 +- 3.04 (95%)
    SPRT  | 30.0+0.30s Threads=1 Hash=64MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 2.50]
    Games | N: 23218 W: 5502 L: 5266 D: 12450
    Penta | [21, 2529, 6274, 2763, 22]
    
    It worked at STC too:
    Elo   | 3.72 +- 3.05 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.94 (-2.25, 2.89) [0.00, 2.50]
    Games | N: 23738 W: 5800 L: 5546 D: 12392
    Penta | [81, 2658, 6173, 2840, 117]
    
    Bench 3985666

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -15,49 +15,49 @@
 
 namespace Search {
     
-  DEFINE_PARAM_S(QsFpMargin, 160, 10);
+  DEFINE_PARAM_S(QsFpMargin, 153, 10);
 
-  DEFINE_PARAM_S(LmrBase, 39, 10);
-  DEFINE_PARAM_S(LmrDiv, 211, 10);
+  DEFINE_PARAM_S(LmrBase, 50, 10);
+  DEFINE_PARAM_S(LmrDiv, 200, 10);
 
-  DEFINE_PARAM_S(StatBonusLinear, 130, 10);
-  DEFINE_PARAM_S(StatBonusMax, 1159, 50);
-  DEFINE_PARAM_S(StatBonusBoostAt, 120, 10);
+  DEFINE_PARAM_S(StatBonusLinear, 134, 10);
+  DEFINE_PARAM_S(StatBonusMax, 1153, 50);
+  DEFINE_PARAM_S(StatBonusBoostAt, 121, 10);
 
-  DEFINE_PARAM_S(RazoringDepthMul, 415, 10);
+  DEFINE_PARAM_S(RazoringDepthMul, 418, 10);
 
   DEFINE_PARAM_S(RfpMaxDepth, 9, 1);
-  DEFINE_PARAM_S(RfpDepthMul, 123, 6);
+  DEFINE_PARAM_S(RfpDepthMul, 125, 6);
 
   DEFINE_PARAM_S(NmpBase, 4, 1);
   DEFINE_PARAM_B(NmpDepthDiv, 4, 1, 21);
-  DEFINE_PARAM_S(NmpEvalDiv, 207, 20);
+  DEFINE_PARAM_S(NmpEvalDiv, 195, 20);
   DEFINE_PARAM_S(NmpEvalDivMin, 4, 1);
 
-  DEFINE_PARAM_S(ProbcutBetaMargin, 229, 10);
+  DEFINE_PARAM_S(ProbcutBetaMargin, 225, 10);
 
   DEFINE_PARAM_S(LmpBase,    3, 1);
 
-  DEFINE_PARAM_S(QsSeeMargin, -25, 15);
-  DEFINE_PARAM_S(PvsQuietSeeMargin, -77, 20);
-  DEFINE_PARAM_S(PvsCapSeeMargin, -132, 20);
+  DEFINE_PARAM_S(QsSeeMargin, -32, 15);
+  DEFINE_PARAM_S(PvsQuietSeeMargin, -87, 20);
+  DEFINE_PARAM_S(PvsCapSeeMargin, -118, 20);
 
-  DEFINE_PARAM_S(EarlyLmrHistoryDiv, 5521, 300);
+  DEFINE_PARAM_S(EarlyLmrHistoryDiv, 5487, 300);
 
-  DEFINE_PARAM_S(FpBase, 182, 10);
+  DEFINE_PARAM_S(FpBase, 184, 10);
   DEFINE_PARAM_S(FpMaxDepth, 8, 1);
-  DEFINE_PARAM_S(FpDepthMul, 111, 6);
+  DEFINE_PARAM_S(FpDepthMul, 107, 6);
 
-  DEFINE_PARAM_S(TripleExtMargin, 130, 10);
-  DEFINE_PARAM_S(DoubleExtMargin, 16, 2);
+  DEFINE_PARAM_S(TripleExtMargin, 129, 10);
+  DEFINE_PARAM_S(DoubleExtMargin, 17, 2);
   DEFINE_PARAM_S(DoubleExtMax, 6, 1);
 
-  DEFINE_PARAM_S(LmrQuietHistoryDiv, 10486, 300);
-  DEFINE_PARAM_S(LmrCapHistoryDiv, 8003, 300);
-  DEFINE_PARAM_S(ZwsDeeperMargin, 79, 5);
+  DEFINE_PARAM_S(LmrQuietHistoryDiv, 10638, 300);
+  DEFINE_PARAM_S(LmrCapHistoryDiv, 8125, 300);
+  DEFINE_PARAM_S(ZwsDeeperMargin, 78, 5);
 
   DEFINE_PARAM_B(AspWindowStartDepth, 4, 4, 34);
-  DEFINE_PARAM_B(AspWindowStartDelta, 11, 5, 45);
+  DEFINE_PARAM_B(AspWindowStartDelta, 13, 5, 45);
   
   bool doingBench = false;
 
--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-12.17";
+const std::string engineVersion = "dev-12.18";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
