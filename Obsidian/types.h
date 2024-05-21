1cfd9dcd71a66839317efd4b377f42e74d7f8d87
gab8192 <gabrilomba57@gmail.com>
2024-05-21 14:49:29 +0200

    Do more LMR at root
    I just didn't like the "3" multiplier
    
    Elo   | 1.02 +- 2.36 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.89 (-2.25, 2.89) [-2.50, 0.50]
    Games | N: 38776 W: 9084 L: 8970 D: 20722
    Penta | [116, 4529, 9984, 4643, 116]
    
    Bench 4630168

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-12.27";
+const std::string engineVersion = "dev-12.28";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
