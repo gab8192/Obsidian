165717ada14335a713e54662c16e24275e5ab5ce
gab8192 <gabrilomba57@gmail.com>
2024-06-19 22:49:06 +0200

    Bench 4072392

--- Obsidian/search.cpp
+++ Obsidian/search.cpp
@@ -980,7 +980,8 @@ namespace Search {
         R += !improving;
 
         // Reduce if we expect to fail high
-        R += 2 * cutNode;
+        if (cutNode)
+          R += 2 - ttPV;
 
         // Clamp to avoid a qsearch or an extension in the child search
         int reducedDepth = std::clamp(newDepth - R, 1, newDepth + 1);
