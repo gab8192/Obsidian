108e5d3a0cee1745023bda095a27e4fb608b874b
gab8192 <gabrilomba57@gmail.com>
2024-07-01 14:07:44 +0200

    Increment version
    Bench 4319693

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-12.37";
+const std::string engineVersion = "13.0";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
