fec54620acf3695f6965604fdd4cf6ec424f12b1
gab8192 <gabrilomba57@gmail.com>
2024-07-09 08:32:23 +0200

    New net
    Elo   | 5.60 +- 3.88 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 2.50]
    Games | N: 15192 W: 3877 L: 3632 D: 7683
    Penta | [57, 1758, 3763, 1919, 99]
    
    Bench 3849023

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net47.bin"
+#define EvalFile "net50.bin"
 
 using namespace SIMD;
 
@@ -30,16 +30,16 @@ namespace NNUE {
   constexpr int HiddenWidth = 1536;
 
   constexpr int KingBucketsScheme[] = {
-    0, 0, 1, 1, 1, 1, 0, 0,
-    2, 2, 3, 3, 3, 3, 2, 2,
-    4, 4, 4, 4, 4, 4, 4, 4, 
-    5, 5, 5, 5, 5, 5, 5, 5, 
-    6, 6, 6, 6, 6, 6, 6, 6, 
-    6, 6, 6, 6, 6, 6, 6, 6, 
-    6, 6, 6, 6, 6, 6, 6, 6, 
-    6, 6, 6, 6, 6, 6, 6, 6
+    0,  1,  2,  3,  3,  2,  1,  0,
+    4,  5,  6,  7,  7,  6,  5,  4,
+    8,  8,  9,  9,  9,  9,  8,  8,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    11, 11, 11, 11, 11, 11, 11, 11, 
+    11, 11, 11, 11, 11, 11, 11, 11, 
+    12, 12, 12, 12, 12, 12, 12, 12, 
+    12, 12, 12, 12, 12, 12, 12, 12, 
   };
-  constexpr int KingBuckets = 7;
+  constexpr int KingBuckets = 13;
 
   constexpr int OutputBuckets = 8;
 
