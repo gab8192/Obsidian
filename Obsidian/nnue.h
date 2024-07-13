3272fc11ae266e8174eca7b43b695d7bc2a19563
gab8192 <gabrilomba57@gmail.com>
2024-07-13 09:03:43 +0200

    Bench 3889395

--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -44,8 +44,8 @@ namespace NNUE {
   constexpr int OutputBuckets = 8;
 
   constexpr int NetworkScale = 400;
-  constexpr int NetworkQA = 255;
-  constexpr int NetworkQB = 64;
+  constexpr int NetworkQA = 128;
+  constexpr int NetworkQB = 128;
   constexpr int NetworkQAB = NetworkQA * NetworkQB;
 
   struct Accumulator {
