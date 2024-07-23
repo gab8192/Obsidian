35af367d97e039fdae2ae05b45d689c5ed5c9ef6
gab8192 <gabrilomba57@gmail.com>
2024-07-23 18:15:27 +0200

    Bench 4013589

--- Obsidian/nnue.cpp
+++ Obsidian/nnue.cpp
@@ -15,9 +15,12 @@ namespace NNUE {
   struct {
     alignas(Alignment) weight_t FeatureWeights[KingBuckets][2][6][64][HiddenWidth];
     alignas(Alignment) weight_t FeatureBiases[HiddenWidth];
-    alignas(Alignment) weight_t OutputWeights[OutputBuckets][2 * HiddenWidth];
+    alignas(Alignment) weight_t OldOutputWeights[2 * HiddenWidth][OutputBuckets];
                        weight_t OutputBias[OutputBuckets];
   } Content;
+
+  alignas(Alignment) weight_t NewOutputWeights[OutputBuckets][2 * HiddenWidth];
+
 
   bool needRefresh(Color side, Square oldKing, Square newKing) {
     const bool oldMirrored = fileOf(oldKing) >= FILE_E;
@@ -169,18 +172,22 @@ namespace NNUE {
 
     memcpy(&Content, gEmbeddedNNUEData, sizeof(Content));
     
+    for (int i = 0; i < 2 * HiddenWidth; i++) {
+      for (int j = 0; j < OutputBuckets; j++) {
+        NewOutputWeights[j][i] = Content.OldOutputWeights[i][j];
+      }
+    }
   }
 
   Score evaluate(Position& pos, Accumulator& accumulator) {
-
-    constexpr int divisor = (32 + OutputBuckets - 1) / OutputBuckets;
-    int outputBucket = (BitCount(pos.pieces()) - 2) / divisor;
+    int numPc = BitCount(pos.pieces());
+    int outputBucket = std::min((63 - numPc) * (32 - numPc) / 225, 7);
 
     Vec* stmAcc = (Vec*) accumulator.colors[pos.sideToMove];
     Vec* oppAcc = (Vec*) accumulator.colors[~pos.sideToMove];
 
-    Vec* stmWeights = (Vec*) &Content.OutputWeights[outputBucket][0];
-    Vec* oppWeights = (Vec*) &Content.OutputWeights[outputBucket][HiddenWidth];
+    Vec* stmWeights = (Vec*) &NewOutputWeights[outputBucket][0];
+    Vec* oppWeights = (Vec*) &NewOutputWeights[outputBucket][HiddenWidth];
 
     const Vec vecZero = vecSetZero();
     const Vec vecQA = vecSet1Epi16(NetworkQA);
--- Obsidian/nnue.h
+++ Obsidian/nnue.h
@@ -3,7 +3,7 @@
 #include "simd.h"
 #include "types.h"
 
-#define EvalFile "net52.bin"
+#define EvalFile "nn.net"
 
 using namespace SIMD;
 
@@ -30,16 +30,17 @@ namespace NNUE {
   constexpr int HiddenWidth = 1536;
 
   constexpr int KingBucketsScheme[] = {
-    0,  1,  2,  3,  3,  2,  1,  0,
-    4,  5,  6,  7,  7,  6,  5,  4,
-    8,  8,  9,  9,  9,  9,  8,  8,
-    10, 10, 10, 10, 10, 10, 10, 10,
-    11, 11, 11, 11, 11, 11, 11, 11, 
-    11, 11, 11, 11, 11, 11, 11, 11, 
-    12, 12, 12, 12, 12, 12, 12, 12, 
-    12, 12, 12, 12, 12, 12, 12, 12, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    
   };
-  constexpr int KingBuckets = 13;
+  constexpr int KingBuckets = 1;
 
   constexpr int OutputBuckets = 8;
 
