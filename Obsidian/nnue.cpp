3ee77f1aa6ed19ecf6b6fb3c2cb1d091d9c020c4
gab8192 <gabrilomba57@gmail.com>
2024-08-10 01:18:37 +0200

    Bench 3347049

--- Obsidian/nnue.cpp
+++ Obsidian/nnue.cpp
@@ -15,7 +15,7 @@ namespace NNUE {
   struct {
     alignas(Alignment) weight_t FeatureWeights[KingBuckets][2][6][64][HiddenWidth];
     alignas(Alignment) weight_t FeatureBiases[HiddenWidth];
-    alignas(Alignment) weight_t OutputWeights[OutputBuckets][2 * HiddenWidth];
+    alignas(Alignment) weight_t OutputWeights[OutputBuckets][HiddenWidth];
                        weight_t OutputBias[OutputBuckets];
   } Content;
 
@@ -176,35 +176,25 @@ namespace NNUE {
     constexpr int divisor = (32 + OutputBuckets - 1) / OutputBuckets;
     int outputBucket = (BitCount(pos.pieces()) - 2) / divisor;
 
-    Vec* stmAcc = (Vec*) accumulator.colors[pos.sideToMove];
-    Vec* oppAcc = (Vec*) accumulator.colors[~pos.sideToMove];
+    weight_t* stmAcc = accumulator.colors[pos.sideToMove];
+    weight_t* oppAcc = accumulator.colors[~pos.sideToMove];
 
-    Vec* stmWeights = (Vec*) &Content.OutputWeights[outputBucket][0];
-    Vec* oppWeights = (Vec*) &Content.OutputWeights[outputBucket][HiddenWidth];
+    weight_t* stmWeights = &Content.OutputWeights[outputBucket][0];
+    weight_t* oppWeights =  &Content.OutputWeights[outputBucket][HiddenWidth];
 
-    const Vec vecZero = vecSetZero();
-    const Vec vecQA = vecSet1Epi16(NetworkQA);
+    int sum = 0;
 
-    Vec sum = vecSetZero();
-    Vec v0, v1;
-
-    for (int i = 0; i < HiddenWidth / WeightsPerVec; ++i) {
-      // Side to move
-      v0 = maxEpi16(stmAcc[i], vecZero); // clip
-      v0 = minEpi16(v0, vecQA); // clip
-      v1 = mulloEpi16(v0, stmWeights[i]); // square
-      v1 = maddEpi16(v1, v0); // multiply with output layer
-      sum = addEpi32(sum, v1); // collect the result
-
-      // Non side to move
-      v0 = maxEpi16(oppAcc[i], vecZero);
-      v0 = minEpi16(v0, vecQA);
-      v1 = mulloEpi16(v0, oppWeights[i]);
-      v1 = maddEpi16(v1, v0);
-      sum = addEpi32(sum, v1);
+    for (int them = 0; them <= 1; ++them) {
+      weight_t* acc = accumulator.colors[pos.sideToMove ^ them];
+      weight_t* weights = &Content.OutputWeights[outputBucket][them * HiddenWidth / 2];
+      for (int i = 0; i < HiddenWidth/2; ++i) {
+        int16_t crelu0 = std::clamp<int16_t>(acc[i], 0, NetworkQA);
+        int16_t crelu1 = std::clamp<int16_t>(acc[i + HiddenWidth/2], 0, NetworkQA);
+        sum += int(crelu0) * int(crelu1) * int(weights[i]);
+      }
     }
 
-    int unsquared = vecHaddEpi32(sum) / NetworkQA + Content.OutputBias[outputBucket];
+    int unsquared = sum / NetworkQA + Content.OutputBias[outputBucket];
 
     return (unsquared * NetworkScale) / NetworkQAB;
   }
