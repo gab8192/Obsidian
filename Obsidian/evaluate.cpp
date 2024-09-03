2d4c4c191ff719aef754889600d828098f98b7f4
gab8192 <gabrilomba57@gmail.com>
2024-09-03 15:14:54 +0200

    Handle ttStaticEval and 50mr scaling correctly
    Idc about completing it. The code is simple and correct
    
    Elo   | 2.15 +- 3.08 (95%)
    SPRT  | 6.0+0.06s Threads=1 Hash=16MB
    LLR   | 1.81 (-2.25, 2.89) [-3.00, 0.00]
    Games | N: 12794 W: 3141 L: 3062 D: 6591
    Penta | [35, 1482, 3296, 1537, 47]
    
    Bench 3166733

--- Obsidian/evaluate.cpp
+++ Obsidian/evaluate.cpp
@@ -13,10 +13,7 @@ namespace Eval {
                + 5 * BitCount(pos.pieces(ROOK))
                + 12 * BitCount(pos.pieces(QUEEN));    
 
-    score = score * (200 + phase) / 256;           
-
-    // Scale down as 50 move rule approaches
-    score = score * (200 - pos.halfMoveClock) / 200;
+    score = score * (200 + phase) / 256;
 
     // Make sure the evaluation does not mix with guaranteed win/loss scores
     score = std::clamp(score, SCORE_TB_LOSS_IN_MAX_PLY + 1, SCORE_TB_WIN_IN_MAX_PLY - 1);
