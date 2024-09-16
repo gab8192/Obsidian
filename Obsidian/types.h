f6d73f066acad7461438e89607509419ae545583
gab8192 <gabrilomba57@gmail.com>
2024-09-16 23:28:11 +0200

    Tune at LTC with 40K games
    STC:
    Elo   | 7.54 +- 3.43 (95%)
    SPRT  | 6.0+0.06s Threads=1 Hash=16MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 2.50]
    Games | N: 10456 W: 2632 L: 2405 D: 5419
    Penta | [31, 1143, 2665, 1346, 43]
    
    The TM of the test above is actually way higher. When I saw such a result, I didn't care to complete the LTC test
    
    Bench 2860933

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-13.14";
+const std::string engineVersion = "dev-13.15";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
