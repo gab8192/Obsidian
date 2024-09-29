d89526f08cc75916c20c94cb0b0d2cab16dd4627
gab8192 <gabrilomba57@gmail.com>
2024-09-29 11:24:12 +0200

    Even less depth in NMP search
    Elo   | 3.02 +- 2.19 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.40 (-2.25, 2.89) [0.00, 2.50]
    Games | N: 24534 W: 6026 L: 5813 D: 12695
    Penta | [47, 2814, 6346, 2999, 61]
    
    Bench 2812757

--- Obsidian/types.h
+++ Obsidian/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-13.22";
+const std::string engineVersion = "dev-13.23";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
