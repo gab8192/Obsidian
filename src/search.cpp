78ca1e039a4941f1991983532987365e7f1c0542
gab8192 <gabrilomba57@gmail.com>
2024-11-01 23:55:46 +0100

    More sensible standpat
    (my instance)
    
    Bench 3110120

--- src/search.cpp
+++ src/search.cpp
@@ -540,8 +540,8 @@ namespace Search {
 
       if (bestScore >= beta) {
         if (! ttHit)
-          ttEntry->store(pos.key, TT::FLAG_LOWER, 0, MOVE_NONE, bestScore, rawStaticEval, false, ply);
-        return bestScore;
+          ttEntry->store(pos.key, TT::NO_FLAG, 0, MOVE_NONE, SCORE_NONE, rawStaticEval, false, ply);
+        return (bestScore + beta) / 2;
       }
       if (bestScore > alpha)
         alpha = bestScore;
