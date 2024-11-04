5ffe4c3e1e5433cf7946b7214659b404611e6a96
gab8192 <gabrilomba57@gmail.com>
2024-11-04 14:28:33 +0100

    Bench 3424817

--- src/search.cpp
+++ src/search.cpp
@@ -862,7 +862,7 @@ namespace Search {
     }
 
     // IIR. Decrement the depth if we expect this search to have bad move ordering
-    if ((IsPV || cutNode) && depth >= 2+2*cutNode && !ttMove)
+    if ((IsPV || cutNode) && depth >= 2+4*cutNode && !ttMove)
       depth--;
 
     if (   !IsPV
