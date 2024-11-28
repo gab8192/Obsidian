f2c314bdedb447c078fb3c8068c0658aca4b40d0
gab8192 <gabrilomba57@gmail.com>
2024-11-28 23:32:16 +0100

    Only update entry age when storing
    Bench 3043611

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-14.07";
+const std::string engineVersion = "dev-14.08";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
