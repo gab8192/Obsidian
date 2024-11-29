ba0f710685fc76b612a5c45d9aae4e10d1643133
gab8192 <gabrilomba57@gmail.com>
2024-11-29 01:27:45 +0100

    bench 3105566

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,7 @@ namespace Search {
 
       // Late move reductions
 
-      if (depth >= 2 && seenMoves > 1 + 2 * IsRoot) {
+      if (depth >= 2 && seenMoves > 1 + IsRoot) {
 
         int R = lmrTable[depth][seenMoves] / (1 + !isQuiet);
 
