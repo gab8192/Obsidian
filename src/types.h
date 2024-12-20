d5f3e454dca9baadbcdf4b4cbcfc6a158afe7164
gab8192 <gabrilomba57@gmail.com>
2024-12-20 02:00:56 +0100

    Use static eval difference to update mainhist
    Elo   | 2.96 +- 1.80 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 36250 W: 8819 L: 8510 D: 18921
    Penta | [93, 4069, 9492, 4378, 93]
    
    Bench 3237986

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-14.18";
+const std::string engineVersion = "dev-14.19";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
