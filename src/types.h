10fb2d563aca748600792b40b3ed84e3e76eff40
gab8192 <gabrilomba57@gmail.com>
2024-12-22 12:37:09 +0100

    More static eval history update
    Elo   | 4.17 +- 2.79 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 1.81 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 14900 W: 3643 L: 3464 D: 7793
    Penta | [35, 1625, 3959, 1788, 43]
    
    Bench 3953345

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-14.21";
+const std::string engineVersion = "dev-14.22";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
