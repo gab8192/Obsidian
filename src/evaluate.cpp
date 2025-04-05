e11fbdc301857b71c64e3e075f90391a0b1172d2
gab8192 <gabrilomba57@gmail.com>
2025-04-05 16:02:20 +0200

    Use pawn count in eval scaling
    STC:
    Elo   | -2.10 +- 2.12 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | -2.27 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 26584 W: 6217 L: 6378 D: 13989
    Penta | [89, 3179, 6875, 3102, 47]
    
    LTC:
    Elo   | 2.04 +- 2.37 (95%)
    SPRT  | 30.0+0.30s Threads=1 Hash=64MB
    LLR   | 1.03 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 19422 W: 4692 L: 4578 D: 10152
    Penta | [20, 2154, 5248, 2270, 19]
    
    Bench 3171302

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -14,12 +14,13 @@ namespace Eval {
     else
       score -= UCI::contemptValue;
 
-    int phase =  3 * BitCount(pos.pieces(KNIGHT))
+    int phase =  2 * BitCount(pos.pieces(PAWN))
+               + 3 * BitCount(pos.pieces(KNIGHT))
                + 3 * BitCount(pos.pieces(BISHOP))
                + 5 * BitCount(pos.pieces(ROOK))
                + 12 * BitCount(pos.pieces(QUEEN));
 
-    score = score * (200 + phase) / 256;
+    score = score * (176 + phase) / 256;
 
     // Make sure the evaluation does not mix with guaranteed win/loss scores
     score = std::clamp(score, SCORE_TB_LOSS_IN_MAX_PLY + 1, SCORE_TB_WIN_IN_MAX_PLY - 1);
