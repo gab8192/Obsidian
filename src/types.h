06c84b115487040737ec05af469db8a089e1ff0e
gab8192 <gabrilomba57@gmail.com>
2025-04-06 02:11:35 +0200

    Halve CH weight when updating insteaed of ordering
    STC:
    Elo   | 0.92 +- 0.99 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 1.63 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 120946 W: 29171 L: 28851 D: 62924
    Penta | [305, 14124, 31310, 14414, 320]
    
    LTC:
    Elo   | 1.33 +- 1.55 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 1.30 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 43856 W: 10523 L: 10355 D: 22978
    Penta | [28, 4798, 12102, 4978, 22]
    
    Bench 3480739

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-15.12";
+const std::string engineVersion = "dev-15.13";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
