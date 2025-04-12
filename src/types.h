1bc1df0ea03a2b141fab32c2c6d9487623c4ce05
gab8192 <gabrilomba57@gmail.com>
2025-04-12 18:27:35 +0200

    Improve how nnz permuting
    Bench 3480739

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-15.13";
+const std::string engineVersion = "dev-15.14";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
