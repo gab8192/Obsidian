ee7cd83e517299b8a5517f801e163382f2517178
gab8192 <gabrilomba57@gmail.com>
2025-05-09 07:27:51 +0200

    Tune Tune Tune Sahur
    STC:
    Elo   | 3.53 +- 2.01 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 29160 W: 7197 L: 6901 D: 15062
    Penta | [71, 3255, 7641, 3533, 80]
    
    LTC:
    Elo   | 4.32 +- 2.21 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.89 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 21860 W: 5498 L: 5226 D: 11136
    Penta | [20, 2317, 5976, 2605, 12]
    
    Bench 3953733

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-15.20";
+const std::string engineVersion = "dev-15.21";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
