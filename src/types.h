aa5b8b21048bd7b6dd7e923324d74d3e4be72971
gab8192 <gabrilomba57@gmail.com>
2025-05-18 22:59:54 +0200

    New net
    STC:
    Elo   | 2.25 +- 1.53 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.91 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 51464 W: 12932 L: 12598 D: 25934
    Penta | [172, 5867, 13317, 6207, 169]
    
    LTC:
    Elo   | 2.11 +- 1.63 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.26 (-2.25, 2.89) [0.00, 2.00]
    Games | N: 40078 W: 10006 L: 9763 D: 20309
    Penta | [37, 4326, 11054, 4601, 21]
    
    Bench 3125283

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-15.22";
+const std::string engineVersion = "dev-15.23";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
