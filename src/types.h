2838ce52c1a3f997cb1175b360cd8c6b2ae6791c
gab8192 <gabrilomba57@gmail.com>
2025-05-21 15:26:04 +0200

    Increment version
    Bench 3125283

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-15.23";
+const std::string engineVersion = "16.0";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
