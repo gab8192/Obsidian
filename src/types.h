780c3dcb6fbc5fedc8e4c683793d2cead237f3c8
gab8192 <gabrilomba57@gmail.com>
2025-06-05 13:08:23 +0200

    Simplify NMP
    Hit rate of cutNode when entering NMP: 98.7%
    Hit rate of cutNode when NMP actually wins: 99.7%
    
    Elo   | 0.20 +- 1.12 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=16MB
    LLR   | 2.22 (-2.25, 2.89) [-2.00, 0.00]
    Games | N: 95630 W: 23626 L: 23570 D: 48434
    Penta | [253, 11318, 24641, 11326, 277]
    
    Bench 3301121

--- src/types.h
+++ src/types.h
@@ -8,7 +8,7 @@
 #include <nmmintrin.h>
 #include <thread>
 
-const std::string engineVersion = "dev-16.06";
+const std::string engineVersion = "dev-16.07";
 
 using Key = uint64_t;
 using Bitboard = uint64_t;
